3e1738a7ea415619bfbe7e4ea0a1d873d6db3269
AlwaysTired-Dannie <daniellemuller.art@gmail.com>
2025-02-19 12:01:12 +0200

    Started hold function

--- Assets/Scripts/Interfaces/IFTBox.cs
+++ Assets/Scripts/Interfaces/IFTBox.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 public class IFTBox : MonoBehaviour, IInteractable
 {
+    public static Action OnOpenBox;
     public void OnClickAction()
     {
         Debug.Log("interacted with FriendTech Box");
